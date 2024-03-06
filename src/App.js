d2b373ce91cc0d498904c6edac74dee2f65ab9d5
Emily Erskine <s00209630@mail.itsligo.ie>
2024-03-06 12:19:15 +0000

    Fixing the Routes so that it's all good

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import './App.css';
 import { Footer } from './CommonComponents/Footer';
 import { Header } from './CommonComponents/Header';
 import Home from './Home/Home';
+import Activities from './Activities/Activities';
+import Resources from './Resources/Resources';
+import Podcast from './Podcast/Podcast';
+import Ezine from './Ezine/Ezine';
+import Contact from './Contact/Contact';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Activities" element={<Activities />} />
+          <Route path="/Resources" element={<Resources />} />
+          <Route path="/Podcast" element={<Podcast />} />
+          <Route path="/Ezine" element={<Ezine />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
