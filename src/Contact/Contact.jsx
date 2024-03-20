9c53a3c6cc660881f93b79f2c1c3f69f7876ceee
Emily Erskine <s00209630@mail.itsligo.ie>
2024-03-20 17:16:11 +0000

    Added the map to the contact page

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ContactForm from './ContactForm';
-
+import MyMap from "./Map";
 
 export default function Contact() {
   return (
     <main className="flex min-h-screen flex-col justify-between p-24">
         <h1 style={{color: '#4E2B72'}}>Contact</h1>
-        <ContactForm />
+        <div className="container" style={{display: 'flex', padding: '2%'}}>
+          <div style={{flex: '1'}}>
+            <MyMap />
+          </div>
+          <div style={{flex: '1'}}>
+            <ContactForm />
+          </div>
+        </div>
     </main>
   );
 }
